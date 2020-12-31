81e2ebbbb3dca80141e8ae07404c232e4a33ba58
ruben <ruben.malchow@gmail.com>
2020-12-31 11:53:31 +0100

    image name

--- build-docker.sh
+++ build-docker.sh
@@ -3,4 +3,4 @@ set -e
 
 docker build -t rmalchow/mopidy:latest -f Dockerfile-mopidy .
 docker login -u "${DH_USER}" -p "${DH_PW}"
-docker push rmalchow/${CI_PROJECT_NAME}:latest
+docker push rmalchow/mopidy:latest
