3396668c1a3d1876e26432ec6b3ab250d55ea691
ruben <ruben.malchow@gmail.com>
2020-12-31 12:02:26 +0100

    some cleanup

--- assets/install/install.sh
+++ assets/install/install.sh
@@ -16,6 +16,8 @@ apt update
 apt install mopidy mopidy-local mopidy-local-sqlite mopidy-mpd snapclient snapserver -y
 cat /etc/mopidy/*
 
+apt-get clean all
+
 pip3 install Mopidy-Iris
 pip3 install Mopidy-Muse
 pip3 install Mopidy-MPD
@@ -35,3 +37,5 @@ rm -rf runit-2.1.2.tar.gz
 cd admin/runit-2.1.2
 package/install
 package/install-man
+
+rm -rf /root/.cache/*
