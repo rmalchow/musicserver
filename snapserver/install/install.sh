a4c3c4477c5bcf7f323c5dd7c5ccde0689a53886
ruben <ruben.malchow@gmail.com>
2021-01-02 16:11:15 +0100

    snapserver dockerfile corrections

--- snapserver/install/install.sh
+++ snapserver/install/install.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-apt-get update
 
+echo "installing basics ... "
+apt-get update
 apt-get install -y wget apt-utils ca-certificates
 
 arch=$(arch)
@@ -19,7 +20,10 @@ else
   exit
 fi
 
+echo "downloading snapserver release ... "
 wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapserver_0.22.0-1_${arch}.deb
+
+echo "installing snapserver release ... "
 dpkg -i snapserver_* || true
 apt-get update
 apt-get -f install -y
