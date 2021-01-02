fd734a9f4a9b666eadbac9b4d7a0ea05dc9421e4
ruben <ruben.malchow@gmail.com>
2021-01-02 12:49:05 +0100

    debug

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+set +x 
 docker login -u "${DH_USER}" -p "${DH_PW}"
 
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/mopidy:latest -f Dockerfile-mopidy .
