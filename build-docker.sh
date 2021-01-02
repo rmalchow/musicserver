73fbf9d0bb5914a5985c85559fc9e3fa06e89e76
ruben <ruben.malchow@gmail.com>
2021-01-02 13:41:14 +0100

    push immediately

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-set +x 
+set +x
 docker login -u "${DH_USER}" -p "${DH_PW}"
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/mopidy:latest -f Dockerfile-mopidy .
-docker push rmalchow/mopidy:latest
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/snapserver:latest -f Dockerfile-snapserver .
-docker push rmalchow/snapserver:latest
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/mopidy:latest -f Dockerfile-mopidy --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/snapserver:latest -f Dockerfile-snapserver --push .
