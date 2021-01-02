1ae15a39e547d4caacdc4571f8a3abb114935440
ruben <ruben.malchow@gmail.com>
2021-01-02 16:53:50 +0100

    building from source now

--- snapserver/install/install.sh
+++ snapserver/install/install.sh
@@ -12,13 +12,12 @@ apt-get install -y \
   libflac8 libogg0 libopus0 libsoxr0 libvorbis0a \
   libvorbisenc2 \
   libasound2-dev libvorbisidec-dev libvorbis-dev libopus-dev libflac-dev libsoxr-dev alsa-utils libavahi-client-dev avahi-daemon libexpat1-dev \
-  gcc make automake git g++
+  gcc make automake git g++ bzip2
 
 mkdir -p /usr/local/src
 cd /usr/local/src
 wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.bz2
 tar -xzf boost_1_75_0.tar.bz2
-
 
 
 echo "downloading snapserver release ... "
@@ -28,6 +27,7 @@ cd snapcast
 git checkout v0.22.0
 git submodule update --init --recursive
 ADD_CFLAGS="-I/usr/local/src/boost_1_75_0" make installserver
+
 
 echo "installing snapserver release ... "
 dpkg -i snapserver_* || true
