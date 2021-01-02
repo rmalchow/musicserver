525b3954a7e6bdbf3c2806b34db627ca4752c017
ruben <ruben.malchow@gmail.com>
2021-01-02 16:09:13 +0100

    snapserver dockerfile corrections

--- snapserver/install/install.sh
+++ snapserver/install/install.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+set -e
+
 apt-get update
 
 apt-get install -y wget apt-utils ca-certificates
 
-wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapserver_0.22.0-1_$(arch | sed s:"x86_64":"amd64":g).deb
+arch=$(arch)
+
+if [ "x86_64" == ${arch} ]; then
+  arch="amd64"
+else if [ "armv7l" == ${arch} ]; then
+  arch="armhf"
+else if [ "aarch64" == ${arch} ]; then
+  arch="armhf"
+else
+  echo "unknow arch: ${arch}"
+  exit
+fi
+
+wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapserver_0.22.0-1_${arch}.deb
 dpkg -i snapserver_* || true
 apt-get update
 apt-get -f install -y
