3000dcfe7363af7c9fb5d63a7bf23c663f616ac7
ruben <ruben.malchow@gmail.com>
2021-01-02 17:08:55 +0100

    mopidy

--- mopidy/install/install.sh
+++ mopidy/install/install.sh
@@ -13,22 +13,22 @@ echo "installing mopidy repo ... "
 apt-key add mopidy.gpg
 cp mopidy.list /etc/apt/sources.list.d/mopidy.list
 
-# apt update
-# echo "installing mopidy packages ... "
-# apt install mopidy mopidy-local mopidy-local-sqlite mopidy-mpd snapclient snapserver -y
-# cat /etc/mopidy/*
-#
-# apt-get clean all
-#
-# echo "installing mopidy modules ... "
-# pip3 install Mopidy-Iris
-# pip3 install Mopidy-Muse
-# pip3 install Mopidy-MPD
-# pip3 install Mopidy-Jellyfin
-# pip3 install Mopidy-TuneIn
-#
-# cp mopidy.conf /etc/mopidy/
-# chmod -R 755 /etc/mopidy
-#
-# rm -rf /root/.cache/*
-# rm -rf /var/lib/apt/lists/*
+apt update
+echo "installing mopidy packages ... "
+apt install mopidy mopidy-local mopidy-local-sqlite mopidy-mpd snapclient snapserver -y
+cat /etc/mopidy/*
+
+apt-get clean all
+
+echo "installing mopidy modules ... "
+pip3 install Mopidy-Iris
+pip3 install Mopidy-Muse
+pip3 install Mopidy-MPD
+pip3 install Mopidy-Jellyfin
+pip3 install Mopidy-TuneIn
+
+cp mopidy.conf /etc/mopidy/
+chmod -R 755 /etc/mopidy
+
+rm -rf /root/.cache/*
+rm -rf /var/lib/apt/lists/*
