c411cc9bc17ed112f068a11200a42f8dd4f63ca5
ruben <ruben.malchow@gmail.com>
2021-01-10 15:14:29 +0100

    add create venv

--- snapcontrol/run.sh
+++ snapcontrol/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-. ./bin/activate
+
+
 
 cd $(dirname ${0})
 
-if [ -d lib ]; then
+if [ -f bin/active ]; then
   echo "apparently, dependencies are already installed"
+  . ./bin/activate
 else
-  echo "installing dpendencies"
+  echo "installing dependencies"
+  python3 -m venv /docker/music/snapcontrol
+  . ./bin/activate
   pip3 install zeroconf flask flask-jsonpify flask-sqlalchemy flask-restful uwsgi requests psutil
 fi
 
