2fc41ed89a0ce8849669876ca329f2ddc7b2a8ec
ruben <ruben.malchow@gmail.com>
2021-01-13 18:32:44 +0100

    rebuild

--- build-docker.sh
+++ build-docker.sh
@@ -3,5 +3,4 @@ set -e
 set +x
 docker login -u "${DH_USER}" -p "${DH_PW}"
 
-docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v5 -t rmalchow/mopidy:latest -f Dockerfile-mopidy --push .
-docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v5 -t rmalchow/snapserver:latest -f Dockerfile-snapserver --push .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 -t rmalchow/mopidy:latest -f Dockerfile-mopidy --push .
